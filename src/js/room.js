e12144970d1338e30eabeaf17b7e31fa240823c9
ashu <jainamritanshu@gmail.com>
2017-06-05 04:14:30 +0530

    Added live video stream elements, generating video url element and peers in the room

--- src/js/room.js
+++ src/js/room.js
@@ -3,6 +3,8 @@ var widthInput = document.getElementById("width-input");
 var heightInput = document.getElementById("height-input");
 var vidFile = document.getElementById("video-file");
 var broadcastURL = document.getElementById("broadcast-url");
+var baseURL = "http://p2psp.org/virtual-room/room/" // Will be changed accordingly
+var peerID = 'xxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);}); // Generating UUID(taking only the first section of the string) according to the RFC4122 version 4(https://www.ietf.org/rfc/rfc4122.txt)
 
 // Filling placeholders after the video has been loaded
 vid.addEventListener("loadedmetadata", function(e){
@@ -27,7 +29,7 @@ vidFile.onchange = function(){
     };
 
 function streamVideo(){
-	// Make a separate function to load the video and call it in the main streaVideo() function
+	// Make a separate function to load the video and call it in the main streamVideo() function
 	console.log(vidFile.value);
 	var source = document.getElementById("video-source");
 	var vidURL = URL.createObjectURL(vidFile.files[0]);
@@ -45,5 +47,25 @@ function copyBroadcastURL(){
 
 // Function to be modified to contain logic of generating url
 function generateURL(){
-	broadcastURL.innerText = "http://p2psp.org";
+	broadcastURL.innerText = baseURL;
 }
+
+// disconnect the peer from the room
+function disconnectPeer(){
+
+}
+// Updating the number of peers and getting stream from the peer on connecting to other peers
+function addPeer(){
+	var peerNum = document.getElementById("peer-num");
+	var peerMediaElements = document.getElementById("peer-media-banner");
+	parseInt(peerNum)+=1;
+	var peerMedia = document.createElement("div");
+	var peerMediaVideo = document.createElement("video");
+	peerMediaSource.setAttribute("height", "150");
+	var peerMediaSource = document.createElement("source");
+	source.src = "../bbb-cbr-1300-frag.mp4"; // to be updated with UserMedia
+	source.id = "user-media-"+peerID;
+	peerMediaVideo.appendChild(source);
+	peerMediaDiv.setAttribute("class", "col s4");
+	peerMediaDiv.appendChild(peerMediaVideo); 
+}	
