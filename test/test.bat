e91836a248eae120533ad3f65f7497bc5f1174a2
joseje95 <32633129+joseje95@users.noreply.github.com>
2018-02-01 12:06:21 +0100

    Add files via upload

---
+++ test/test.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+
+::Execute python script
+START C:\Virtual-Room-master\src\signal-server\signal_server.py > NUL %*
+
+:: Run Web server
+START C:\xampp\php\php.exe -S 0.0.0.0:3000 C:\Virtual-Room-master\test\routing.php > NUL %*
